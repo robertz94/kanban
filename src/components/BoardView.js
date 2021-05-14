0d68d2e22ce4945cab3c6a90b7a039294839172b
Robert Zhang <robertzhang94@gmail.com>
2021-05-14 19:32:46 +0200

    Logic all seems to work

--- src/components/BoardView.js
+++ src/components/BoardView.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useHistory } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { setBoard1, setBoard2, setBoard3 } from '../actions/HomeActions'
 import Issue from '../models/Issue'
+import Board from '../models/Board'
 
 const BoardView = () => {
     let location = useLocation();
+    let history = useHistory();
     let [board, setBoard] = useState(location.state.board)
     let [id, setId] = useState(location.state.id)
+    let [dragIssue, setDragIssue] = useState([])
     let priorities = [1,2,3,4]
     let dispatch = useDispatch();
 
@@ -21,28 +24,47 @@ const BoardView = () => {
                 priority = i
             }
         }
+        appendIssueToColumn(category, issueTitle, priority)
+    }
+
+    const appendIssueToColumn = (category, issueTitle, priority) => {
         let issue = new Issue (issueTitle, priority, category);
+        let newBoard = {...board}
+        newBoard.columns[category].issues = newBoard.columns[category].issues.concat(issue)
+        setBoard(newBoard)
+        let boards = JSON.parse(localStorage.getItem('boards'));
         if (id === 'board1') {
-            let newBoard = {...board}
-            newBoard.columns[category].issues = newBoard.columns[category].issues.concat(issue)
-            setBoard(newBoard)
-            let boards = JSON.parse(localStorage.getItem('boards'));
             boards[0]=board;
             localStorage.setItem('boards', JSON.stringify(boards));
             dispatch(setBoard1(board))
         } else if (id === 'board2') {
-            let newBoard = {...board}
-            newBoard.columns[category].issues = newBoard.columns[category].issues.concat(issue)
-            setBoard(newBoard)
-            let boards = JSON.parse(localStorage.getItem('boards'));
             boards[1]=board;
             localStorage.setItem('boards', JSON.stringify(boards));
             dispatch(setBoard2(board))
         } else if (id === 'board3') {
-            let newBoard = {...board}
-            newBoard.columns[category].issues = newBoard.columns[category].issues.concat(issue)
-            setBoard(newBoard)
-            let boards = JSON.parse(localStorage.getItem('boards'));
+            boards[2]=board;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard3(board))
+        }
+    }
+
+    const removeIssueFromColumn = () => {
+        let category = dragIssue[0];
+        let index = dragIssue[1];
+        let newBoard = {...board};
+        let length = newBoard.columns[category].issues.length
+        newBoard.columns[category].issues = newBoard.columns[category].issues.slice(0, index).concat((newBoard.columns[category].issues.slice(index, length-1)))
+        setBoard(newBoard)
+        let boards = JSON.parse(localStorage.getItem('boards'));
+        if (id === 'board1') {
+            boards[0]=board;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard1(board))
+        } else if (id === 'board2') {
+            boards[1]=board;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard2(board))
+        } else if (id === 'board3') {
             boards[2]=board;
             localStorage.setItem('boards', JSON.stringify(boards));
             dispatch(setBoard3(board))
@@ -60,31 +82,135 @@ const BoardView = () => {
         }
     }
 
+    const dragstartHandler = (event) => {
+        let column = event.target.parentElement.parentElement.id;
+        let issue = event.target.id;
+        let theIssue = board.columns[column].issues[issue]
+        console.log(issue)
+        setDragIssue([column, issue, theIssue])
+    }
+
+    const dragOverHandler = (event) => {
+        event.preventDefault();
+        console.log(event.target.id)
+    }
+
+    const dropHandler = (event) => {
+        event.preventDefault();
+        let category;
+        if(event.target.id === 'trashCan'){
+            removeIssueFromColumn();
+        } 
+        else {
+         if (event.target.nodeName === 'LI') {
+            category = event.target.parentElement.parentElement;
+            appendIssueToColumn(category.id, dragIssue[2].title, dragIssue[2].priority)
+            removeIssueFromColumn()
+        } else if (event.target.nodeName === 'DIV') {
+            category = event.target;
+            appendIssueToColumn(category.id, dragIssue[2].title, dragIssue[2].priority)
+           removeIssueFromColumn()
+        } else {
+            throw new Error ('Place is not meant to be a drop area')
+        }
+    }
+    }
+
+    const renameBoard = () => {
+        let newName = document.getElementById('newBoardName').value;
+        let newBoard = {...board, name: newName }
+        setBoard({...board, name: newName })
+        console.log(board)
+        console.log(id)
+        let boards = JSON.parse(localStorage.getItem('boards'));
+        if (id === 'board1') {
+            boards[0]=board;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard1(board))
+        } else if (id === 'board2') {
+            boards[1]=board;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard2(board))
+        } else if (id === 'board3') {
+            boards[2]=board;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard3(board))
+        }
+    }
+    
+    const displayRenameForm = () => {
+        let formVisibility = document.getElementById('renameBoardSection').style.display;
+        if (formVisibility === 'none') {
+            document.getElementById('renameBoardSection').style.display = 'block';
+        } else if (formVisibility === 'block') {
+            document.getElementById('renameBoardSection').style.display = 'none';
+        } else {
+            document.getElementById('renameBoardSection').style.display = 'block';
+        }
+    }
+
+    const deleteBoard = () => {
+        let emptyBoard = new Board('untitled', [])
+        let boards = JSON.parse(localStorage.getItem('boards'));
+        if (id === 'board1') {
+            boards[0]=emptyBoard;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard1(emptyBoard))
+        } else if (id === 'board2') {
+            boards[1]=emptyBoard;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard2(emptyBoard))
+        } else if (id === 'board3') {
+            boards[2]=emptyBoard;
+            localStorage.setItem('boards', JSON.stringify(boards));
+            dispatch(setBoard3(emptyBoard))
+        }
+        history.push({
+            pathname: '/',
+        })
+    }
+
+    const displayDeleteForm = () => {
+        let formVisibility = document.getElementById('deleteBoardSection').style.display;
+        if (formVisibility === 'none') {
+            document.getElementById('deleteBoardSection').style.display = 'block';
+        } else if (formVisibility === 'block') {
+            document.getElementById('deleteBoardSection').style.display = 'none';
+        } else {
+            document.getElementById('deleteBoardSection').style.display = 'block';
+        }
+    }
+
     return (
         <div className={"boardViewContainer"}>
+            
             <div id={"boardViewNavBar"}>
                 <ul id={"boardViewUl"}>
                 <li className={"boardViewLi"}>{board.name}</li>
-                <li className={"boardViewLi"}>Menu</li>
+                <li id={"renameBtn"} className={"boardViewLi"} onClick={displayRenameForm}>Rename</li>
                 <li id={"addIssueBtn"} className={"boardViewLi"} onClick={displayAddIssueForm}>Add</li>
+                <li id={"deleteBtn"} className={"boardViewLi"} onClick={displayDeleteForm}>Delete</li>
                 </ul>
             </div>
             <div className={"columnsContainer"}>
                 {board.columns.map((column, i) =>
-                    <div key={i} id={column.name} className={"kanbanColumn"}>
+                    <div key={i} id={i} className={"kanbanColumn"} onDrop={dropHandler} onDragOver={dragOverHandler}>
                         <div id={"columnName"}>{column.name}</div>
-                        <div id={"columnIssues"}>
+                        <ul id={"columnIssues"} onDrop={dropHandler} onDragOver={dragOverHandler}>
                             {column.issues
                                 .sort((a, b) => a.priority - b.priority)
                                 .map((issue, j) => 
-                                <div key={j} className={'issue'}>
+                                <li key={j} id={j} className={'issue'} draggable={'true'} onDragStart={dragstartHandler}>
                                     <h3>{issue.title} - {issue.priority}</h3>
-                                </div>
+                                </li>
                             )}
-                        </div>
+                        </ul>
                     </div>            
                 )}
             </div>
+            <div id={'trashCan'} onDrop={dropHandler} onDragOver={dragOverHandler}>
+                    <h2>Trash</h2>
+                </div>
             <div id={"createIssueContainer"} className={"createIssueContainer"}>
                 <h3>Create a new Issue</h3>
                 <div>
@@ -110,7 +236,20 @@ const BoardView = () => {
                 </div>
                 <button onClick={addIssue}>Add Issue</button>
             </div>
+            <div id={"renameBoardSection"} className={"renameBoardSection"}>
+                    <h2>Rename Kanban Board</h2>
+                    <label>New Board Name</label>
+                    <input type={'text'} name={'newBoardName'} id={'newBoardName'} placeholder={'New name...'}/>
+                    <button id={"renameBtn"} onClick={renameBoard}>Rename</button>
+                    <button id={"renameCancelBtn"} onClick={displayRenameForm}>Cancel</button>
+            </div>
+            <div id={"deleteBoardSection"} className={"deleteBoardSection"}>
+                <h2>Are you sure you want to delete the board. All progress will be lost!</h2>
+                <button id={"deleteBtn"} onClick={deleteBoard}>Delete</button>
+                <button id={"deleteCancelBtn"} onClick={displayDeleteForm}>Cancel</button>
+            </div>
         </div>
+
     )
 }
 
